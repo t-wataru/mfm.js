08b3ff6ffa504ab6f7eb408843d4249792bd5873
marihachi <marihachi0620@gmail.com>
2021-05-31 19:40:00 +0900

    remove debug code

--- src/internal/parser/index.ts
+++ src/internal/parser/index.ts
@@ -220,13 +220,11 @@ function matchFnVer1(ctx: MatcherContext, pos: number): MatcherResult {
 	const argsResult = matchFnArgs(ctx, pos + offset);
 	if (argsResult.success) {
 		offset += argsResult.length;
-		console.log('argsResult.length:', argsResult.length);
 	}
 	const args = argsResult.success ? argsResult.data : { };
 
 	// _
 	if (!/^[ 　\t\u00a0]$/.test(ctx.input[pos + offset])) {
-		console.log('_ fail');
 		return fail();
 	}
 	offset++;
@@ -243,12 +241,9 @@ function matchFnVer1(ctx: MatcherContext, pos: number): MatcherResult {
 
 	// "]"
 	if (ctx.input[pos + offset] != ']') {
-		console.log('] fail');
 		return fail();
 	}
 	offset += 1;
-
-	console.log('success matchFnVer1');
 
 	return success(offset, FN(name, args, content));
 }
@@ -256,14 +251,11 @@ function matchFnVer1(ctx: MatcherContext, pos: number): MatcherResult {
 function matchFnArgs(ctx: MatcherContext, pos: number): MatcherResult {
 	let mainOffset = 0;
 
-	console.log('enter matchFnArgs');
-
 	if (ctx.input[pos + mainOffset] != '.') return fail();
 	mainOffset++;
 
 	const head = matchFnArg(ctx, pos + mainOffset);
 	if (!head.success) {
-		console.log('head fail');
 		return fail();
 	}
 	mainOffset += head.length;
@@ -271,21 +263,18 @@ function matchFnArgs(ctx: MatcherContext, pos: number): MatcherResult {
 	const tailsMatcher = repetition(0, (_, p) => {
 		let offset = 0;
 		if (ctx.input[p + offset] != ',') {
-			console.log(', fail');
 			return fail();
 		}
 		offset++;
 		const tail = matchFnArg(ctx, p + offset);
 		if (!tail.success) return fail();
 		offset += tail.length;
-		console.log('tails offset', offset);
+
 		return success(offset, tail.data);
 	});
 	const tails = tailsMatcher(ctx, pos + mainOffset);
 	if (!tails.success) return fail();
 	mainOffset += tails.length;
-
-	console.log('success matchFnArgs');
 
 	return success(mainOffset, [head.data, ...tails.data]);
 };
@@ -293,11 +282,8 @@ function matchFnArgs(ctx: MatcherContext, pos: number): MatcherResult {
 function matchFnArg(ctx: MatcherContext, pos: number): MatcherResult {
 	let mainOffset = 0;
 
-	console.log('enter matchFnArg');
-
 	const kResult = /^[a-z0-9_]+/i.exec(ctx.input.substr(pos + mainOffset));
 	if (kResult == null) {
-		console.log('kResult fail');
 		return fail();
 	}
 	const key = kResult[0];
@@ -305,23 +291,19 @@ function matchFnArg(ctx: MatcherContext, pos: number): MatcherResult {
 
 	function valueMatcher(_: MatcherContext, p: number): MatcherResult {
 		let offset = 0;
-		console.log('enter valueMatcher');
 
 		if (ctx.input[p + offset] != '=') {
-			console.log('= fail');
 			return fail();
 		}
 		offset++;
 
 		const vResult = /^[a-z0-9_]+/i.exec(ctx.input.substr(p + offset));
 		if (vResult == null) {
-			console.log('v fail');
 			return fail();
 		}
 		const v = vResult[0];
 		offset += v.length;
 
-		console.log('valueMatcher success');
 		return success(offset, v);
 	}
 	const valueResult = valueMatcher(ctx, pos + mainOffset);
@@ -329,9 +311,6 @@ function matchFnArg(ctx: MatcherContext, pos: number): MatcherResult {
 	if (valueResult.success) {
 		mainOffset += valueResult.length;
 	}
-
-	console.log('success matchFnArg');
-	console.log('matchFnArg length:', mainOffset);
 
 	return success(mainOffset, { k: key, v: value });
 };
