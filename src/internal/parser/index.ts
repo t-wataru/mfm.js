47029300cfa68d43724736dd019426826e0b3f14
marihachi <marihachi0620@gmail.com>
2021-05-31 19:37:20 +0900

    [exp] implement fn v1

--- src/internal/parser/index.ts
+++ src/internal/parser/index.ts
@@ -1,5 +1,5 @@
 import { mergeText } from '../util';
-import { BOLD, MfmInline, MfmNode, MfmPlainNode } from '../../node';
+import { BOLD, FN, MfmInline, MfmNode, MfmPlainNode } from '../../node';
 import { choice, fail, MatcherContext, MatcherResult, repetition, success } from '../matcher';
 
 export function parseFull(input: string): MfmNode[] {
@@ -52,7 +52,21 @@ export function parsePlain(input: string): MfmPlainNode[] {
 
 function matchInline(ctx: MatcherContext, pos: number): MatcherResult {
 	const matcher = choice([
+		matchEmojiCode,
+		matchUnicodeEmoji,
+		matchBig,
 		matchBold,
+		matchSmall,
+		matchItalic,
+		matchStrike,
+		matchInlineCode,
+		matchMathInline,
+		matchMention,
+		matchHashtag,
+		matchUrl,
+		matchFnVer2,
+		matchLink,
+		matchFnVer1,
 		matchText
 	]);
 	return matcher(ctx, pos);
@@ -174,10 +188,153 @@ function matchLink(ctx: MatcherContext, pos: number): MatcherResult {
 	return fail();
 }
 
+/**
+ * fnVer1 = "[" name:$([a-z0-9_]i)+ args:fnArgs? _ content:fnContentPart+ "]"
+ * fnArgs = "." head:fnArg tails:("," arg:fnArg { return arg; })*
+ * fnArg
+ * 	= k:$([a-z0-9_]i)+ "=" v:$([a-z0-9_.]i)+
+ * 	/ k:$([a-z0-9_]i)+
+ * fnContentPart = !("]") i:inlineWithoutFn { return i; }
+ * 
+*/
 function matchFnVer1(ctx: MatcherContext, pos: number): MatcherResult {
-	// TODO
-	return fail();
+	let offset = 0;
+
+	// "["
+	if (ctx.input[pos + offset] != '[') {
+		return fail();
+	}
+	offset += 1;
+
+	// name
+	let name = '';
+	while (/^[a-z0-9_]$/i.test(ctx.input[pos + offset])) {
+		name += ctx.input[pos + offset];
+		offset++;
+	}
+	if (name.length == 0) {
+		return fail();
+	}
+
+	// args (option)
+	const argsResult = matchFnArgs(ctx, pos + offset);
+	if (argsResult.success) {
+		offset += argsResult.length;
+		console.log('argsResult.length:', argsResult.length);
+	}
+	const args = argsResult.success ? argsResult.data : { };
+
+	// _
+	if (!/^[ 　\t\u00a0]$/.test(ctx.input[pos + offset])) {
+		console.log('_ fail');
+		return fail();
+	}
+	offset++;
+
+	// content
+	const contentMatcher = repetition(1, (_, p) => {
+		if (ctx.input[p] == ']') return fail();
+		return matchInline(ctx, p);
+	});
+	const contentResult = contentMatcher(ctx, pos + offset);
+	if (!contentResult.success) return contentResult;
+	offset += contentResult.length;
+	const content = mergeText(contentResult.data) as MfmInline[];
+
+	// "]"
+	if (ctx.input[pos + offset] != ']') {
+		console.log('] fail');
+		return fail();
+	}
+	offset += 1;
+
+	console.log('success matchFnVer1');
+
+	return success(offset, FN(name, args, content));
 }
+
+function matchFnArgs(ctx: MatcherContext, pos: number): MatcherResult {
+	let mainOffset = 0;
+
+	console.log('enter matchFnArgs');
+
+	if (ctx.input[pos + mainOffset] != '.') return fail();
+	mainOffset++;
+
+	const head = matchFnArg(ctx, pos + mainOffset);
+	if (!head.success) {
+		console.log('head fail');
+		return fail();
+	}
+	mainOffset += head.length;
+
+	const tailsMatcher = repetition(0, (_, p) => {
+		let offset = 0;
+		if (ctx.input[p + offset] != ',') {
+			console.log(', fail');
+			return fail();
+		}
+		offset++;
+		const tail = matchFnArg(ctx, p + offset);
+		if (!tail.success) return fail();
+		offset += tail.length;
+		console.log('tails offset', offset);
+		return success(offset, tail.data);
+	});
+	const tails = tailsMatcher(ctx, pos + mainOffset);
+	if (!tails.success) return fail();
+	mainOffset += tails.length;
+
+	console.log('success matchFnArgs');
+
+	return success(mainOffset, [head.data, ...tails.data]);
+};
+
+function matchFnArg(ctx: MatcherContext, pos: number): MatcherResult {
+	let mainOffset = 0;
+
+	console.log('enter matchFnArg');
+
+	const kResult = /^[a-z0-9_]+/i.exec(ctx.input.substr(pos + mainOffset));
+	if (kResult == null) {
+		console.log('kResult fail');
+		return fail();
+	}
+	const key = kResult[0];
+	mainOffset += key.length;
+
+	function valueMatcher(_: MatcherContext, p: number): MatcherResult {
+		let offset = 0;
+		console.log('enter valueMatcher');
+
+		if (ctx.input[p + offset] != '=') {
+			console.log('= fail');
+			return fail();
+		}
+		offset++;
+
+		const vResult = /^[a-z0-9_]+/i.exec(ctx.input.substr(p + offset));
+		if (vResult == null) {
+			console.log('v fail');
+			return fail();
+		}
+		const v = vResult[0];
+		offset += v.length;
+
+		console.log('valueMatcher success');
+		return success(offset, v);
+	}
+	const valueResult = valueMatcher(ctx, pos + mainOffset);
+	const value = valueResult.success ? valueResult.data : true;
+	if (valueResult.success) {
+		mainOffset += valueResult.length;
+	}
+
+	console.log('success matchFnArg');
+	console.log('matchFnArg length:', mainOffset);
+
+	return success(mainOffset, { k: key, v: value });
+};
 
 function matchFnVer2(ctx: MatcherContext, pos: number): MatcherResult {
 	// TODO
