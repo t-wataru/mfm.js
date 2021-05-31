dc9b9a4f1c3561843d513b77eaae8ece5877ff7d
marihachi <marihachi0620@gmail.com>
2021-05-31 16:29:39 +0900

    add matchers

--- src/internal/parser/index.ts
+++ src/internal/parser/index.ts
@@ -8,8 +8,27 @@ export function parseFull(input: string): MfmNode[] {
 	};
 
 	const matcher = repetition(0, choice([
-		matchInline
-		// TODO
+		matchQuote, // block
+		matchCodeBlock, // block
+		matchMathBlock, // block
+		matchCenter, // block
+		matchEmojiCode,
+		matchUnicodeEmoji,
+		matchBig,
+		matchBold,
+		matchSmall,
+		matchItalic,
+		matchStrike,
+		matchInlineCode,
+		matchMathInline,
+		matchMention,
+		matchHashtag,
+		matchUrl,
+		matchFnVer2,
+		matchLink,
+		matchFnVer1,
+		matchSearch, // block
+		matchText
 	]));
 	const result = matcher(ctx, 0);
 
@@ -22,7 +41,8 @@ export function parsePlain(input: string): MfmPlainNode[] {
 	};
 
 	const matcher = repetition(0, choice([
-		// TODO
+		matchEmojiCode,
+		matchUnicodeEmoji,
 		matchText
 	]));
 	const result = matcher(ctx, 0);
@@ -36,6 +56,50 @@ function matchInline(ctx: MatcherContext, pos: number): MatcherResult {
 		matchText
 	]);
 	return matcher(ctx, pos);
+}
+
+//
+// blocks
+//
+
+function matchQuote(ctx: MatcherContext, pos: number): MatcherResult {
+	// TODO
+	return fail();
+}
+
+function matchSearch(ctx: MatcherContext, pos: number): MatcherResult {
+	// TODO
+	return fail();
+}
+
+function matchCodeBlock(ctx: MatcherContext, pos: number): MatcherResult {
+	// TODO
+	return fail();
+}
+
+function matchMathBlock(ctx: MatcherContext, pos: number): MatcherResult {
+	// TODO
+	return fail();
+}
+
+function matchCenter(ctx: MatcherContext, pos: number): MatcherResult {
+	// TODO
+	return fail();
+}
+
+function matchEmojiCode(ctx: MatcherContext, pos: number): MatcherResult {
+	// TODO
+	return fail();
+}
+
+function matchUnicodeEmoji(ctx: MatcherContext, pos: number): MatcherResult {
+	// TODO
+	return fail();
+}
+
+function matchBig(ctx: MatcherContext, pos: number): MatcherResult {
+	// TODO
+	return fail();
 }
 
 /*
@@ -63,6 +127,61 @@ function matchBold(ctx: MatcherContext, pos: number): MatcherResult {
 	offset += 2;
 
 	return success(offset, BOLD(mergeText(contentResult.data) as MfmInline[]));
+}
+
+function matchSmall(ctx: MatcherContext, pos: number): MatcherResult {
+	// TODO
+	return fail();
+}
+
+function matchItalic(ctx: MatcherContext, pos: number): MatcherResult {
+	// TODO
+	return fail();
+}
+
+function matchStrike(ctx: MatcherContext, pos: number): MatcherResult {
+	// TODO
+	return fail();
+}
+
+function matchInlineCode(ctx: MatcherContext, pos: number): MatcherResult {
+	// TODO
+	return fail();
+}
+
+function matchMathInline(ctx: MatcherContext, pos: number): MatcherResult {
+	// TODO
+	return fail();
+}
+
+function matchMention(ctx: MatcherContext, pos: number): MatcherResult {
+	// TODO
+	return fail();
+}
+
+function matchHashtag(ctx: MatcherContext, pos: number): MatcherResult {
+	// TODO
+	return fail();
+}
+
+function matchUrl(ctx: MatcherContext, pos: number): MatcherResult {
+	// TODO
+	return fail();
+}
+
+function matchLink(ctx: MatcherContext, pos: number): MatcherResult {
+	// TODO
+	return fail();
+}
+
+function matchFnVer1(ctx: MatcherContext, pos: number): MatcherResult {
+	// TODO
+	return fail();
+}
+
+function matchFnVer2(ctx: MatcherContext, pos: number): MatcherResult {
+	// TODO
+	return fail();
 }
 
 /*
